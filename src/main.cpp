ced5738ac1a7a21546bb0f2acc397311f44cfb01
Lucas Teixeira <lucax.tei.t@gmail.com>
2024-06-05 17:59:44 -0300

    a lot

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
+// Station config
+#include <Adafruit_Sensor.h>
 #include <Arduino.h>
+#include <Wire.h>
+
+#include "esp_sleep.h"
 
 // RTC - Real Time Clock Config
 #include <RTClib.h>
-#include <Wire.h>
 RTC_DS1307 rtc;
 
 // SD Card config
 #include <SPI.h>
 #include <SdFat.h>
-
 SdFat SD;
 FsFile myFile;
 #define FILE_WRITE (O_RDWR | O_CREAT | O_AT_END)
@@ -16,39 +19,35 @@ FsFile myFile;
 #define SD_CS_PIN 5
 #define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SD_SCK_MHZ(15))
 
-// Station config
-#include <Adafruit_Sensor.h>
+// BME280 CONFIG
+#include <Adafruit_BME280.h>
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME280 bme;  // I2C
+float bme_temperature, bme_altitude, bme_pressure, bme_humidity;
+
+// AM2320 CONFIG
 #include <Adafruit_AM2320.h>
-#include <BME280.h>
-#include <BME280I2C.h>
-#include <EnvironmentCalculations.h>
-#include <Wire.h>
-#include "esp_sleep.h"
-
-#define BME_SDA 21  // Replace with your SDA pin
-#define BME_SCL 22  // Replace with your SCL pin
-
 Adafruit_AM2320 am2320 = Adafruit_AM2320();
-BME280I2C bme;
-// Variables used in reading temp,pressure and humidity (BME280)
-float bme_temperature, bme_humidity, bme_pressure;
-// variaveis do AM2320
-float temp_2320, humidity_2320;
+float am_temperature, am_humidity;
 
+// BAT WATCH
 float VBAT;
 #define VBAT_PIN 33
 
+// ULP SLEEP TIMER
 uint64_t UpdateInterval =
     1 * 30 * 1000000;  // e.g. 0.33 * 60 * 1000000; // Sleep time
 RTC_DATA_ATTR int bootCount = 0;
 
 // DECLARATIONS
-void WriteToCSV(DateTime, float, float, float, float, float);
+void WriteToCSV(DateTime, float, float, float, float, float, float);
 void INIT_SD();
 void INIT_RTC();
+void INIT_BME();
 void Deep_Sleep_Now();
 void readdata();
 void printdata();
+void led_indicator();
 
 void setup() {
     // Open serial communications and wait for port to open:
@@ -56,66 +55,97 @@ void setup() {
     while (!Serial) {
         ;  // wait for serial port to connect. Needed for native USB port only
     }
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
     pinMode(VBAT_PIN, INPUT);
     ++bootCount;
 
     // INITIALIZES
-    // INIT_SD();
-    // INIT_RTC();
+    INIT_SD();
+    delay(500);
+    INIT_RTC();
+    delay(500);
+    INIT_BME();
+    delay(500);
 }
 
 void loop() {
     Wire.begin();
+
     // LOGIC
     delay(2000);
     readdata();
     printdata();
-    // VBAT = map(analogRead(VBAT_PIN), 0, 4094, 0, 3.7);
+
+    VBAT = map(analogRead(VBAT_PIN), 0, 4094, 0, 4.2);
     Serial.println();
     Serial.println(VBAT);
-    // DateTime now = rtc.now();
 
-    // WriteToCSV(now, temperature, humidity, pressure / 100, temp_2320,
-    //            humidity_2320);
+    // LOGGING
+    DateTime now = rtc.now();
 
-    float wake_time = (float)millis() /
-                      float(1000);  // Find out how long since the ESP rebooted
+    WriteToCSV(now, bme_temperature, bme_humidity, bme_pressure, bme_pressure,
+               am_temperature, am_humidity);
 
+    // Find out how long since the ESP rebooted
+    float wake_time = (float)millis() / float(1000);
+
+    // ULP - SLEEP
     Serial.print("Wake Time = ");
     Serial.print(wake_time);
     Serial.println(" seconds");
-
     delay(100);
-
     Deep_Sleep_Now();
 }
 
 void INIT_SD() {
+    led_indicator();
     Serial.print("Initializing SD card...");
     if (!SD.begin(SD_CONFIG)) {
+        led_indicator();
+        led_indicator();
+        led_indicator();
         SD.initErrorPrint(&Serial);
         Deep_Sleep_Now();
     }
     Serial.println("initialization done.");
+    led_indicator();
 }
 
 void INIT_RTC() {
+    led_indicator();
     if (!rtc.begin()) {
+        led_indicator();
+        led_indicator();
+        led_indicator();
         Serial.println("Couldn't find RTC");
         Deep_Sleep_Now();
-        while (1) {
-            ;
-        }
     }
 
     if (!rtc.isrunning()) {
+        led_indicator();
+        led_indicator();
+        led_indicator();
         Serial.println("RTC is not running! Setting the time.");
         // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
+    led_indicator();
+}
+void INIT_BME() {
+    led_indicator();
+    bool status;
+    status = bme.begin(0x76);
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+    led_indicator();
 }
 
-void WriteToCSV(DateTime timestamp, float temp, float humi, float press,
-                float tmp_2320, float humi_2320) {
+void WriteToCSV(DateTime timestamp, float bme_t, float bme_h, float bme_alt,
+                float bme_pres, float AM_t, float AM_hu) {
+    led_indicator();
     char fileName[25];
     sprintf(fileName, "WS1_%02d%02d%04d.csv", timestamp.day(),
             timestamp.month(), timestamp.year());
@@ -126,14 +156,17 @@ void WriteToCSV(DateTime timestamp, float temp, float humi, float press,
 
     // Check if the file exists or create it
     if (!SD.exists(fileName)) {
+        led_indicator();
+        led_indicator();
+        led_indicator();
         // If the file doesn't exist, create it
         myFile = SD.open(fileName, FILE_WRITE);
 
         if (myFile) {
             // Write header to the file (assuming CSV header format)
             myFile.println(
-                "Timestamp;temperature;humidity;pressure;temp_2320;humidity_"
-                "2320");
+                "Timestamp;bme_temperature;bme_humidity;bme_pressure;bme_"
+                "altitude;am2320_temperature;am2320_humidity");
             myFile.close();
             Serial.println("Header written to file: " + String(fileName));
         } else {
@@ -143,24 +176,33 @@ void WriteToCSV(DateTime timestamp, float temp, float humi, float press,
     }
 
     // Open the file for appending data
+    led_indicator();
     myFile = SD.open(fileName, FILE_WRITE);
 
     if (myFile) {
-        String data = String(timeBuffer) + ";" + String(temp) + ";" +
-                      String(humi) + ";" + String(press) + ";" +
-                      String(tmp_2320) + ";" + String(humi_2320);
+        led_indicator();
+        String data = String(timeBuffer) + ";" + String(bme_t) + ";" +
+                      String(bme_h) + ";" + String(bme_pres) + ";" +
+                      String(bme_alt) + ";" + String(AM_t) + ";" +
+                      String(AM_hu);
         data.replace(".", ",");
         // Write sensor data to the file
         myFile.println(data);
         myFile.close();
         Serial.println("Data written to file: " + String(fileName));
         Serial.println(data);
+        led_indicator();
     } else {
+        led_indicator();
+        led_indicator();
+        led_indicator();
         Serial.println("Error opening file: " + String(fileName));
     }
 }
 
 void Deep_Sleep_Now() {
+    led_indicator();
+    led_indicator();
     esp_sleep_enable_timer_wakeup(UpdateInterval);
     Serial.println("ESP is tired, going to sleep.");
     Serial.flush();
@@ -170,26 +212,44 @@ void Deep_Sleep_Now() {
 }
 
 void printdata() {
-    // printa os dados do BME280
+    // BME280 READOUT
+    led_indicator();
     Serial.print("Air temperature [°C]: ");
     Serial.println(bme_temperature);
-    Serial.print("Humidity [%]: ");
-    Serial.println(int(bme_humidity));
+    Serial.print("Altitude [m]: ");
+    Serial.println(int(bme_altitude));
     Serial.print("Barometric pressure [hPa]: ");
     Serial.println(bme_pressure);
-    delay(1000);
+    Serial.print("BME Humidity [%]: ");
+    Serial.println(bme_humidity);
+    led_indicator();
+    // AM2320 READOUT
+    led_indicator();
     Serial.print("AM2320 temperature [C]: ");
-    Serial.println(temp_2320);
+    Serial.println(am_temperature);
     Serial.print("AM2320 Humidity [%]: ");
-    Serial.println(humidity_2320);
+    Serial.println(am_humidity);
+    led_indicator();
 }
 
 void readdata() {
     // Reading BME280 sensor
-    bme.read(bme_pressure, bme_temperature, bme_humidity, BME280::TempUnit_Celsius,
-             BME280::PresUnit_Pa);
-    // Reading AM2320 Sensor
+    led_indicator();
+    bme_temperature = bme.readTemperature();
+    bme_pressure = bme.readPressure() / 100.0F;
+    bme_altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    bme_humidity = bme.readHumidity();
+    led_indicator();
 
-    temp_2320 = am2320.readTemperature();
-    humidity_2320 = am2320.readHumidity();
+    // Reading AM2320 Sensor
+    led_indicator();
+    am_temperature = am2320.readTemperature();
+    am_humidity = am2320.readHumidity();
+    led_indicator();
+}
+
+void led_indicator() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
 }
